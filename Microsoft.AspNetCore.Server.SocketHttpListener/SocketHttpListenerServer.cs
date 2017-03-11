96ce81d024b0ef900e09d48a74ac6b3b4f6aba4d
Frank Laub <frank.laub@gmail.com>
2017-03-10 19:02:34 -0800

    Using Microsoft.AspNetCore.Server.HttpSys as a guide to avoid unhandled exceptions from crashing the entire process.

--- Microsoft.AspNetCore.Server.SocketHttpListener/SocketHttpListenerServer.cs
+++ Microsoft.AspNetCore.Server.SocketHttpListener/SocketHttpListenerServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Http.Features;
@@ -13,6 +14,8 @@ namespace Microsoft.AspNetCore.Server.SocketHttpListener
 	{
 		private readonly HttpListener _listener;
 		private readonly ILogger _logger;
+		private readonly Action<object> _processRequest;
+		private IHttpApplication<object> _application;
 
 		public IFeatureCollection Features { get; } = new FeatureCollection();
 
@@ -20,10 +23,14 @@ namespace Microsoft.AspNetCore.Server.SocketHttpListener
 			IOptions<SocketHttpListenerOptions> options,
 			ILoggerFactory loggerFactory)
 		{
+			_processRequest = ProcessRequestAsync;
 			_logger = loggerFactory.CreateLogger(typeof(SocketHttpListenerServer).FullName);
 			_listener = new HttpListener(
 				new SocketHttpListenerLogger(loggerFactory),
-				options.Value.Certificate);
+				options.Value.Certificate)
+			{
+				OnContext = OnContext
+			};
 
 			Features.Set<IServerAddressesFeature>(new ServerAddressesFeature());
 		}
@@ -33,42 +40,7 @@ namespace Microsoft.AspNetCore.Server.SocketHttpListener
 			if (application == null)
 				throw new ArgumentNullException(nameof(application));
 
-			_listener.OnContext = async context =>
-			{
-				var featureContext = new FeatureContext(context);
-				var appContext = application.CreateContext(featureContext.Features);
-				try
-				{
-					await application.ProcessRequestAsync(appContext);
-					await featureContext.OnStart();
-					application.DisposeContext(appContext, null);
-					await featureContext.OnCompleted();
-				}
-				catch (Exception ex)
-				{
-					_logger.LogError(0, ex, "Unhandled exception in OnContext");
-
-					application.DisposeContext(appContext, ex);
-
-					try
-					{
-						context.Response.Headers.Clear();
-						context.Response.StatusCode = 500;
-						context.Response.ContentLength64 = 0;
-
-						using (var writer = new StreamWriter(context.Response.OutputStream))
-							writer.Write(ex.ToString());
-					}
-					catch
-					{
-						// ignored
-					}
-				}
-				finally
-				{
-					context.Response.Close();
-				}
-			};
+			_application = new ApplicationWrapper<TContext>(application);
 
 			_listener.Prefixes.Clear();
 			foreach (var address in Features.Get<IServerAddressesFeature>().Addresses)
@@ -82,6 +54,97 @@ namespace Microsoft.AspNetCore.Server.SocketHttpListener
 		public void Dispose()
 		{
 			_listener.Stop();
+		}
+
+		private void OnContext(HttpListenerContext context)
+		{
+			try
+			{
+				var ignored = Task.Factory.StartNew(_processRequest, context);
+			}
+			catch (Exception ex)
+			{
+				// Request processing failed to be queued in threadpool
+				// Log the error message, release throttle and move on
+				_logger.LogError(0, ex, "OnContext");
+			}
+		}
+
+		private async void ProcessRequestAsync(object contextObj)
+		{
+			var context = (HttpListenerContext) contextObj;
+			try
+			{
+				object appContext = null;
+				var featureContext = new FeatureContext(context);
+				try
+				{
+					appContext = _application.CreateContext(featureContext.Features);
+					try
+					{
+						await _application.ProcessRequestAsync(appContext);
+						await featureContext.OnStart();
+						_application.DisposeContext(appContext, null);
+					}
+					finally
+					{
+						await featureContext.OnCompleted();
+					}
+				}
+				catch (Exception ex)
+				{
+					_logger.LogError(0, ex, "ProcessRequestAsync");
+					if (featureContext.Features.Get<IHttpResponseFeature>().HasStarted)
+					{
+						context.Response.Abort();
+					}
+					else
+					{
+						// We haven't sent a response yet, try to send a 500 Internal Server Error
+						context.Response.Headers.Clear();
+						context.Response.StatusCode = 500;
+						context.Response.ContentLength64 = 0;
+
+						using (var writer = new StreamWriter(context.Response.OutputStream))
+							writer.Write(ex.ToString());
+					}
+					_application.DisposeContext(appContext, ex);
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(0, ex, "Outer ProcessRequestAsync");
+				context.Response.Abort();
+			}
+			finally
+			{
+				context.Response.Close();
+			}
+		}
+
+		private class ApplicationWrapper<TContext> : IHttpApplication<object>
+		{
+			private readonly IHttpApplication<TContext> _application;
+
+			public ApplicationWrapper(IHttpApplication<TContext> application)
+			{
+				_application = application;
+			}
+
+			public object CreateContext(IFeatureCollection contextFeatures)
+			{
+				return _application.CreateContext(contextFeatures);
+			}
+
+			public void DisposeContext(object context, Exception exception)
+			{
+				_application.DisposeContext((TContext) context, exception);
+			}
+
+			public Task ProcessRequestAsync(object context)
+			{
+				return _application.ProcessRequestAsync((TContext) context);
+			}
 		}
 	}
 }
